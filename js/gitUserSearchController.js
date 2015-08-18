9122d06bc7f64385b007654faad04ee0ec37a4ed
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-18 22:27:14 +0100

    stubbed results can be displayed

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,21 @@
 githubUserSearch.controller('GitUserSearchController', [function() {
 
+var self = this;
+
+  self.doSearch = function() {
+    self.searchResult = {
+      "items": [
+        {
+          "login": "GBouffard",
+          "avatar_url": "https://avatars0.githubusercontent.com/u/10553003?v=3",
+          "html_url": "https://github.com/GBouffard"
+        }, 
+        {
+          "login": "PaulWallis42",
+          "avatar_url": "https://avatars3.githubusercontent.com/u/10485555?v=3",
+          "html_url": "https://github.com/PaulWallis42"
+        }
+      ]
+    };
+  };
 }]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -11,4 +11,24 @@ describe('GitUserSearchController', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
+
+  describe('when searching for a user', function() {
+  var items = [
+    {
+      "login": "GBouffard",
+      "avatar_url": "https://avatars0.githubusercontent.com/u/10553003?v=3",
+      "html_url": "https://github.com/GBouffard"
+    }, 
+    {
+      "login": "PaulWallis42",
+      "avatar_url": "https://avatars3.githubusercontent.com/u/10485555?v=3",
+      "html_url": "https://github.com/PaulWallis42"
+    }
+  ];
+
+    it('displays search results', function() {
+      ctrl.doSearch();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
 });
