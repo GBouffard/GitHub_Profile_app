7cc3ee39d742a72db38922ca18cd8a5338597337
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-20 13:14:15 +0100

    linking our factory to our controller

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,11 +1,10 @@
-githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
-
-var self = this;
-var searchResource = $resource('https://api.github.com/search/users?access_token=47c9e10c94239841baa1a65a44a0f972813c257d');
+githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
+  var self = this;
 
   self.doSearch = function() {
-    self.searchResult = searchResource.get(
-      { q: self.searchTerm }
-    );
+    Search.query(self.searchTerm)
+      .then(function(response) {
+        self.searchResult = response.data;
+      })
   };
 }]);
