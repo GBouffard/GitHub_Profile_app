812abc74ab92732d2eddcadc3369af5e8d40f5bf
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-20 12:54:43 +0100

    testing the promise returned by our http request

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,5 +1,14 @@
 githubUserSearch.factory('Search', ['$http', function($http) {
- return {
-    query: 'foo'
+  var queryUrl = 'https://api.github.com/search/users?access_token=47c9e10c94239841baa1a65a44a0f972813c257d';
+  return {
+    query: function(searchTerm) {
+      return $http({
+        url: queryUrl,
+        method: 'GET',
+        params: {
+          'q': searchTerm
+        }
+      }); 
+    }
   }
 }]);
--- test/searchfactory.spec.js
+++ test/searchfactory.spec.js
@@ -2,14 +2,42 @@ describe('factory: Search', function() {
 
   var search;
 
+  var items = [
+    {
+      "login": "GBouffard",
+      "avatar_url": "https://avatars0.githubusercontent.com/u/10553003?v=3",
+      "html_url": "https://github.com/GBouffard"
+    }, 
+    {
+      "login": "PaulWallis42",
+      "avatar_url": "https://avatars3.githubusercontent.com/u/10485555?v=3",
+      "html_url": "https://github.com/PaulWallis42"
+    }
+  ];
+
   beforeEach(module('GitUserSearch'));
 
   beforeEach(inject(function(Search) {
     search = Search;
   }));
 
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend
+    httpBackend
+      .when("GET", "https://api.github.com/search/users?q=hello")
+      .respond(
+        { items: items }
+      );
+  }));
+
   it('responds to query', function() {
     expect(search.query).toBeDefined();
   });
 
+  it('returns search results', function() {
+    search.query('hello')
+      .then(function(response) {
+        expect(response.data).toEqual(items)
+      })  
+  });
 });
