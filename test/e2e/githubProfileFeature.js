7867934d1b63081b9ac57aa61d838ef9e8238ab3
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-21 18:20:40 +0100

    Protractor test - correct profiles are found on search terms

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -3,4 +3,15 @@ describe('Github Profile finder', function() {
     browser.get('http://localhost:8080');
     expect(browser.getTitle()).toEqual('Guillaume Github user search');
   });
+
+  it('finds profiles', function() {
+    browser.get('http://localhost:8080');
+
+    element(by.model('searchCtrl.searchTerm')).sendKeys('GBouffard');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('user.login')).getText()).toEqual('GBouffard');
+  });
 });
+
+
