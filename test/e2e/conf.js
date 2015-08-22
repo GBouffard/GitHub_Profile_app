470ab5e6813f4b6b147a602861776e0e61508bb9
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-23 00:11:45 +0100

    Testing on multiple browsers: chrome & firefox

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,10 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['githubProfileFeature.js'],
-  directConnect: false,
-  firefoxPath: null,
-  baseUrl: "http://localhost:8080",
-  capabilities: {
+  multiCapabilities: [{
     browserName: 'firefox'
-  }
+  }, {
+    browserName: 'chrome'
+  }]
 }
